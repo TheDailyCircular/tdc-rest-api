14f560c3d373de906b6d2bc69dcf56500c6a723e
MasumBhuiyan <masumbhuiyan577@gmail.com>
2020-07-21 15:41:36 +0600

    messageController initialized

---
+++ src/main/java/com/thedailycircular/tdc/controller/MessageController.java
@@ -0,0 +1,12 @@
+package com.thedailycircular.tdc.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("message")
+public class MessageController {
+
+}
