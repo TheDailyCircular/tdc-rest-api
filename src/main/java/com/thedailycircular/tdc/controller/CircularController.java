e8dc8a2b5811fa40e62ae4a2baeba1e93a3fb296
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-07-23 00:12:28 +0600

    Error Mapper and Circular Controller

---
+++ src/main/java/com/thedailycircular/tdc/controller/CircularController.java
@@ -0,0 +1,39 @@
+package com.thedailycircular.tdc.controller;
+
+import com.thedailycircular.tdc.model.Circular;
+import com.thedailycircular.tdc.service.CircularServices;
+import com.thedailycircular.tdc.validation.ValidationErrorMappingServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping(path = "circular/")
+public class CircularController {
+
+    @Autowired
+    private CircularServices circularServices;
+
+    @Autowired
+    private ValidationErrorMappingServices validationErrorMappingServices;
+
+    @PostMapping("create")
+    public ResponseEntity<?> create(@Valid @RequestBody Circular circular, BindingResult result) {
+        ResponseEntity<?> errorMap = validationErrorMappingServices.mapValidationErrors(result);
+        if (errorMap != null) {
+            return errorMap;
+        }
+        return new ResponseEntity<>(circularServices.saveOrUpdate(circular), HttpStatus.CREATED);
+    }
+
+    @GetMapping("all")
+    public List<Circular> getAll() {
+        return circularServices.getAll();
+    }
+}
