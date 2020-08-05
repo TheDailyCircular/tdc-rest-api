d4f2f8d87c3976ecc6589677e5d16d807e20a09d
kmtusher97 <kmtusher97@gmail.com>
2020-08-05 22:19:20 +0600

    Pagination and filter request parameter added to getAll() api

--- src/main/java/com/thedailycircular/tdc/controller/CircularController.java
+++ src/main/java/com/thedailycircular/tdc/controller/CircularController.java
@@ -5,13 +5,15 @@ import com.thedailycircular.tdc.service.CircularServices;
 import com.thedailycircular.tdc.validation.CircularValidator;
 import com.thedailycircular.tdc.validation.ValidationErrorMappingServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
+import java.util.Date;
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -37,9 +39,11 @@ public class CircularController {
         return new ResponseEntity<>(circularServices.saveOrUpdate(circular), HttpStatus.CREATED);
     }
 
-    @GetMapping("/all")
-    public List<Circular> getAll() {
-        return circularServices.getAll();
+    @GetMapping("/circulars")
+    public Page<Circular> getAll(@RequestParam Optional<Date> date,
+                                 @RequestParam Optional<Integer> page,
+                                 @RequestParam Optional<Integer> size) {
+        return circularServices.getAll(date.orElse(new Date()), page.orElse(0), size.orElse(5));
     }
 
     @GetMapping("/{id}")
